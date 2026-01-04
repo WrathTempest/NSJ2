ea7dcb5fd9a37918e94a1a6f746610bfb3955cb4
WrathTempest <emmeneses@up.edu.ph>
2026-01-04 20:08:07 +0800

    Add project files.

---
+++ NSJ2/Main.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NSJ2
+{
+    // TODO Review this file and update to your own requirements.
+
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class Main : BaseUnityPlugin
+    {
+        private const string MyGUID = "izayoixx";
+        private const string PluginName = "NSJ2_CustomMod";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGUID);
+        public static ManualLogSource Log = new ManualLogSource(PluginName);
+
+        /// <summary>
+        /// Initialise the configuration settings and patch methods
+        /// </summary>
+        private void Awake()
+        {
+            // Apply all of our patches
+            Harmony.PatchAll();
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loaded.");
+            Log = Logger;
+        }
+
+    }
+}
---
+++ NSJ2/QingGongGraphMgr_Patches.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using SweetPotato;
+
+namespace NSJ2
+{
+    [HarmonyPatch(typeof(QingGongGraphMgr))]
+    internal class QingGongGraphMgr_Patches
+    {
+        [HarmonyPatch(nameof(QingGongGraphMgr.UpGradeNode))]
+        [HarmonyPrefix]
+        public static void Upgrade_Patch(QingGongGraphMgr __instance, int nodeGroup, bool bCheck, ref bool bForceUpdate, bool bForceFromScript, bool showTip)
+        {
+            bForceUpdate = true; //force it to always upgrade
+            int num = -1;
+            int num2 = -1;
+            if (__instance.m_DirActiveNode.ContainsKey(nodeGroup))
+            {
+                num = __instance.m_DirActiveNode[nodeGroup];
+                QingGong_node infoById = QingGong_node.GetInfoById(num);
+                if (infoById != null)
+                {
+                    num2 = infoById.nextid;
+                }
+            }
+            else
+            {
+                num = -1;
+                QingGong_node infoLevelOne = QingGong_node.GetInfoLevelOne(nodeGroup);
+                if (infoLevelOne != null)
+                {
+                    num2 = infoLevelOne.id;
+                }
+            }
+            QingGong_node infoById2 = QingGong_node.GetInfoById(num);
+            QingGong_node infoById3 = QingGong_node.GetInfoById(num2);
+            if (infoById3 == null)
+            {
+                return;
+            }
+            WorldManager.Instance.m_PlayerEntity.m_AttriManager.ModAttriField(AttriType.GanWu, infoById3.needGanWu, false, -1, -1, true, true, false, true, false);
+            Main.Log.LogInfo("Qinggong Upgrade Patch Triggered!");
+        }
+    }
+}
